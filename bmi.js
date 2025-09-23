0b3ab6369842538fe984355a1676deabf7e33aa4
agm129134-bit <agm129134@gmail.com>
2025-09-23 16:12:37 +0800

    防呆

--- bmi.js
+++ bmi.js
@@ -1,7 +1,7 @@
 const readline = require("readline-sync");
 
-var weight=readline.question("U R weight(KG)");
-var height=readline.question("U R height(CM)");
+var weight=readline.questionFloat("U R weight(KG)");
+var height=readline.questionFloat("U R height(CM)");
 var bmi = weight/((height/100)**2);
 
 console.log("Hello! Your BMI value is "+bmi);
