f9ae7ddf8b55f7cb4684f644c0d82dd534197641
agm129134-bit <agm129134@gmail.com>
2025-12-02 15:15:32 +0800

    20251202

--- maze.js
+++ maze.js
@@ -22,21 +22,23 @@ class Point{
 var start=new Point(1,1);
 var end = new Point(8,10);
 var Stack=[];
+var Direction=[[-1,0],[0,-1],[1,0],[0,1]].sort((a,b)=>{return Math.random()-0.5});//permutation
 
 var CP=start;//currentPosition
 do{
     MAZE[CP.row][CP.col]=2;
+    drawBoard();
     //4 directions
-    if(MAZE[CP.row-1][CP.col]==0){//up
+    if(MAZE[CP.row+Direction[0][0]][CP.col+Direction[0][1]]==0){//dir0
         Stack.push(CP);
-        CP = new Point(CP.row-1,CP.col);
-    }else if(MAZE[CP.row][CP.col-1]==0){ //left
+        CP = new Point(CP.row+Direction[0][0],CP.col+Direction[0][1]);
+    }else if(MAZE[CP.row+Direction[1][0]][CP.col+Direction[1][1]]==0){ //dir1
         Stack.push(CP);
-        CP = new Point(CP.row,CP.col-1);
-    }else if(MAZE[CP.row+1][CP.col]==0){//down
+        CP = new Point(CP.row+Direction[1][0],CP.col+Direction[1][1]);
+    }else if(MAZE[CP.row+1][CP.col]==0){//dir2
         Stack.push(CP);
         CP = new Point(CP.row+1,CP.col);
-    }else if(MAZE[CP.row][CP.col+1]==0){//right
+    }else if(MAZE[CP.row][CP.col+1]==0){//dir3
         Stack.push(CP);
         CP = new Point(CP.row,CP.col+1);
     }else{
@@ -53,8 +55,6 @@ do{
 //}while(!(CP.row==end.row && CP.col==end.col));
 }while(CP.row!=end.row || CP.col!=end.col);
 
-if(CP.row!==end.row && CP.col!==end.col){
-    MAZE[CP.row][CP.col]=2;
-    console.log("Found the exit!");
-}
-console.table(MAZE);
+// if(Stack.length>0){
+//   //print path 
+//   Stack.push(end);
